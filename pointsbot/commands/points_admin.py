41aa27fd22952066eb7d11f2f1b0cb3e812acb7f
Robert Xiu <xiurobert@gmail.com>
2022-05-24 11:59:52 +0800

    chore: suppress IDE warnings
    thanks pycharm

--- pointsbot/commands/points_admin.py
+++ pointsbot/commands/points_admin.py
@@ -1,6 +1,6 @@
-# noinspection PyPackageRequirements
 from typing import Union, Optional
 
+# noinspection PyPackageRequirements
 import discord
 # noinspection PyPackageRequirements
 from discord.ext import commands
